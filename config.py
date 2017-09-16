542f69a62a2652976292e76281d7c80392cbda9b
basimsahaf <bssahaf@uwaterloo.ca>
2017-09-16 14:05:08 -0400

    Os version, application version done

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 FIREBASE_URL = 'https://htn-threatmonitor.firebaseio.com'
 SERVER_ID = '-KuAJDJfSw-FYI7tFUIa'
+email = 'basimsahaf62@gmail.com'
+FIREBASE_SECRET = 'OrbXyqLOPRHI76mpc7cv8UUUVGuI8Uo5XALqt4SN'
+ID = 'MEnfSGdd6PTC9nsBT2z74jhB3KZ2'
