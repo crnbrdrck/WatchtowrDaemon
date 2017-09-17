03f29ebe3edfcb0968f3bc4225825007a1542bdf
Ubuntu <ubuntu@ip-172-31-30-60.ec2.internal>
2017-09-17 07:54:36 +0000

    Commands working on server

--- watchtowr/daemon.py
+++ watchtowr/daemon.py
@@ -4,6 +4,7 @@ from firebase.firebase import FirebaseApplication, FirebaseAuthentication
 from threading import Thread
 import re
 from shlex import split
+from shelve import open as sh_open
 import subprocess
 from sys import stderr
 from .config import SERVER_ID
@@ -16,23 +17,44 @@ firebase = FirebaseApplication(FIREBASE_URL,authentication)
 def check_commands():
     # Check for new commands to be run by this daemon process
     commands = firebase.get('commands', SERVER_ID)
+    if not commands:
+        return
     # firebase.delete('commands', SERVER_ID)
     # Run through each of the commands and run them in a subprocess
-    for command in commands:
-        if not command['run_time']:
-            p = subprocess.Popen(split(command['cmd']), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = p.communicate()
-            firebase.update('commands', command['name'], {
-                'stdout': stdout.decode(),
-                'stderr': stderr.decode(),
-                'exit_code': p.returncode,
-                'run_time': datetime.now().strftime('%H:%M:%S %d/%m/%y')
-            })
+    for command, data in commands.items():
+        if not data['run_time']:
+            safe = False
+            for CMD in WHITELISTED_COMMANDS:
+                if CMD in data['cmd']:
+                    safe = True
+                    break
+            if safe:
+                print('Running cmd "%s"' % command)
+                p = subprocess.Popen(split(data['cmd']), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout, stderr = p.communicate()
+                firebase.put('commands/%s' % SERVER_ID, command, {
+                    'stdout': stdout.decode(),
+                    'stderr': stderr.decode(),
+                    'exit_code': p.returncode,
+                    'run_time': datetime.now().strftime('%H:%M:%S %d/%m/%y'),
+                    'cmd': data['cmd']
+                 })
+            else:
+                firebase.put('commands/%s' % SERVER_ID, command, {
+                    'stdout': 'COMMAND NOT ALLOWED',
+                    'stderr': 'COMMAND NOT ALLOWED',
+                    'exit_code': -1,
+                    'cmd': data['cmd']
+                })
 
 
-def startDaemon(service_calls):
+def startDaemon():
     # Start a thread to check the firebase api for commands this system needs to run
     Thread(target=check_commands).start()
+    with sh_open('service_calls') as data:
+        data.setdefault('service_calls', 0)
+        data['service_calls'] += 1
+        service_calls = data['service_calls']
     # Every 30 minutes, do the application list part
     if service_calls % 6 == 0:
         print('WatchTowr Daemon starting up', file=stderr)
