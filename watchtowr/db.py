088656df529482f95243c8cb984e18b4ebb0d8f3
Ubuntu <ubuntu@ip-172-31-30-60.ec2.internal>
2017-09-17 11:23:30 +0000

    Applications now added one at a time

--- watchtowr/db.py
+++ watchtowr/db.py
@@ -13,7 +13,8 @@ def update_server(os_version, applications):
     key = 'servers/' + server_id
     firebase.put(key, 'os_version', os_version)
     firebase.delete(key, 'applications')
-    firebase.post(key + '/applications', [{application: applications[application]} for application in applications])
+    for application in applications:
+        firebase.post(key + '/applications', {application: applications[application]})
 
 
 def register_server(user_id, server_name):
